108cf8439f781f3fa073a9ca808540f351d61ddf
KaanHGunay <kaangunay16@gmail.com>
2018-10-13 17:28:40 +0300

    ANN

--- Part 8 - Deep Learning/Volume 1 - Supervised Deep Learning/Section 39 - Artificial Neural Networks (ANN)/ANN.py
+++ Part 8 - Deep Learning/Volume 1 - Supervised Deep Learning/Section 39 - Artificial Neural Networks (ANN)/ANN.py
@@ -34,15 +34,19 @@ X_test = sc.transform(X_test)
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers import Dropout # Rastgele bazı katmanları çalıştırmaz ve aşırı öğrenmenin önüne geçer
+# Genellikle 1. ve 2. katmana uygulanması daha doğru olur (Aşırı öğrenmenin genellikle kaynağı bu katmanlardır.)
 
 # Initializing ANN
 classifier = Sequential()
 
 # Adding the input layer and first hidden layer
 classifier.add(Dense(kernel_initializer = 'uniform', input_dim = 11, activation = 'relu', units = 6)) # relu = rectifier algoritması
+classifier.add(Dropout(p = 0.1))
 
 # Adding second hidden layer
 classifier.add(Dense(kernel_initializer = 'uniform', activation = 'relu', units = 6))
+classifier.add(Dropout(p = 0.1))
 
 # Adding the output layer
 classifier.add(Dense(kernel_initializer = 'uniform', activation = 'sigmoid', units = 1)) # sigmoid = sigmoid alogritması
@@ -66,3 +70,59 @@ cm = confusion_matrix(y_test, y_pred)
 
 # Tek bir örnekten predict alma 
 new_prediction = classifier.predict(sc.transform(np.array([[0, 0, 600, 1, 40, 3, 60000, 2, 1, 1, 50000]])))
+
+# Evaluating ANN
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from keras.models import Sequential
+from keras.layers import Dense
+def build_classifier():
+    classifier = Sequential()
+    classifier.add(Dense(kernel_initializer = 'uniform', input_dim = 11, activation = 'relu', units = 6))
+    classifier.add(Dense(kernel_initializer = 'uniform', activation = 'relu', units = 6))
+    classifier.add(Dense(kernel_initializer = 'uniform', activation = 'sigmoid', units = 1))
+    classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return classifier
+
+classifier = KerasClassifier(build_fn = build_classifier, 
+                             batch_size = 10, 
+                             epochs = 100)
+
+accuracies = cross_val_score(estimator = classifier, 
+                             X = X_train, 
+                             y = y_train,
+                             cv = 10
+                             # n_jobs = -1  # Windows ile ilgili problem paralel hesaplama yapmıyor
+                             )
+mean = accuracies.mean()
+variance = accuracies.std()
+
+# Parameter Tuning
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+from keras.models import Sequential
+from keras.layers import Dense
+def build_classifier(optimizer, kernel_initializer, activation):
+    classifier = Sequential()
+    classifier.add(Dense(units = 6, kernel_initializer = kernel_initializer, activation = activation, input_dim = 11))
+    classifier.add(Dense(units = 6, kernel_initializer = kernel_initializer, activation = activation))
+    classifier.add(Dense(units = 1, kernel_initializer = kernel_initializer, activation = 'sigmoid'))
+    classifier.compile(optimizer = optimizer, loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return classifier
+
+classifier = KerasClassifier(build_fn = build_classifier)
+
+parameters = {'batch_size': [25, 32],
+              'epochs': [50, 60],
+              'kernel_initializer' : ['glorot_uniform', 'uniform'],
+              'activation' : ['elu', 'relu'],
+              'optimizer': ['adam', 'rmsprop']}
+
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameters,
+                           scoring = 'accuracy',
+                           cv = 10)
+
+grid_search = grid_search.fit(X_train, y_train)
+best_parameters = grid_search.best_params_
+best_accuracy = grid_search.best_score_
