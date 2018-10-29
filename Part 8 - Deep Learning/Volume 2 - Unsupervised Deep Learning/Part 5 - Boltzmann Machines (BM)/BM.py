d7ec0057d0c7e8b55720e52cc19cc57066af2c45
KaanHGunay <kaangunay16@gmail.com>
2018-10-29 21:07:55 +0300

    BM

--- Part 8 - Deep Learning/Volume 2 - Unsupervised Deep Learning/Part 5 - Boltzmann Machines (BM)/BM.py
+++ Part 8 - Deep Learning/Volume 2 - Unsupervised Deep Learning/Part 5 - Boltzmann Machines (BM)/BM.py
@@ -38,3 +38,76 @@ def convert(data):
 
 training_set = convert(training_set)
 test_set = convert(test_set)
+
+# Converting the data into Torch tensors
+training_set = torch.FloatTensor(training_set)
+test_set = torch.FloatTensor(test_set)
+
+# Converting the ratings into binary ratings 1 (Liked) and 0 (Not Liked)
+training_set[training_set == 0] = -1 # oylanmamış filmleri -1 yapıyoruz
+training_set[training_set == 1] = 0 # 1 veya 2 verilmişse 0 (beğenmedim) yapıyoruz
+training_set[training_set == 2] = 0
+training_set[training_set >= 3] = 1 # 3, 4 veya 5 verilmişse 1 (beğendim) yapıyoruz
+
+test_set[test_set == 0] = -1 
+test_set[test_set == 1] = 0 
+test_set[test_set == 2] = 0
+test_set[test_set >= 3] = 1
+
+# Creating the architecture of Neural Network
+class RBM():
+    def __init__(self, nv, nh): # nv => number of visible nodes, nh => number of hidden nodes
+        self.W = torch.randn(nh, nv) # Weight - Ağırlıkların tanımlanması
+        self.a = torch.randn(1, nh) # Bias - Gizli katmanlar için Yönelimin tanımlanması, sayılar torch tarafından eğitim sonunda kesiinleşecek
+        self.b = torch.randn(1, nv) # Bias - Açık katmanlar için Yönelimin tanımlanması, sayılar torch tarafından eğitim sonunda kesiinleşecek
+    def sample_h(self, x):
+        wx = torch.mm(x, self.W.t())
+        activation = wx + self.a.expand_as(wx)
+        p_h_given_v = torch.sigmoid(activation)
+        return p_h_given_v, torch.bernoulli(p_h_given_v)
+    def sample_v(self, y):
+        wy = torch.mm(y, self.W)
+        activation = wy + self.b.expand_as(wy)
+        p_v_given_h = torch.sigmoid(activation)
+        return p_v_given_h, torch.bernoulli(p_v_given_h)
+    def train(self, v0, vk, ph0, phk):
+        self.W += (torch.mm(v0.t(), ph0) - torch.mm(vk.t(), phk)).t()
+        self.b += torch.sum((v0 - vk), 0)
+        self.a += torch.sum((ph0 - phk), 0)
+
+nv = len(training_set[0]) # kaç özellikle giriş yapılacak, 1 kullanıcının tüm puanlamaları
+nh = 100 # Gizli katmanda kaç özellik tespit edilsin
+batch_size = 10 # Kaç örnekte bir ağırlılarda güncelleme yapılacak. onlineda 1 olması gerkiyor
+rbm = RBM(nv, nh)
+
+# Training the RBM
+nb_epoch = 10
+for epoch in range(1, nb_epoch + 1):
+    train_loss = 0
+    s = 0.
+    for id_user in range(0, nb_users - batch_size, batch_size):
+        vk = training_set[id_user:id_user+batch_size]
+        v0 = training_set[id_user:id_user+batch_size]
+        ph0,_ = rbm.sample_h(v0)
+        for k in range(10):
+            _,hk = rbm.sample_h(vk)
+            _,vk = rbm.sample_v(hk)
+            vk[v0<0] = v0[v0<0]
+        phk,_ = rbm.sample_h(vk)
+        rbm.train(v0, vk, ph0, phk)
+        train_loss += torch.mean(torch.abs(v0[v0>=0] - vk[v0>=0]))
+        s += 1.
+    print('epoch: '+str(epoch)+' loss: '+str(train_loss/s))
+
+# Testing the RBM
+test_loss = 0
+s = 0.
+for id_user in range(nb_users):
+    v = training_set[id_user:id_user+1]
+    vt = test_set[id_user:id_user+1]
+    if len(vt[vt>=0]) > 0:
+        _,h = rbm.sample_h(v)
+        _,v = rbm.sample_v(h)
+        test_loss += torch.mean(torch.abs(vt[vt>=0] - v[vt>=0]))
+        s += 1.
+print('test loss: '+str(test_loss/s))
